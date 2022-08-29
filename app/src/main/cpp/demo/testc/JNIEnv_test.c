7044d048171d62d5c77d8fc20b0ae096c76651b1
shixin <zhongyang.shi@uworks-gd.cc>
2022-08-29 10:13:20 +0800

    细节修改

--- app/src/main/cpp/demo/testc/JNIEnv_test.c
+++ app/src/main/cpp/demo/testc/JNIEnv_test.c
@@ -85,6 +85,7 @@ int getNextRandomValue(void) {
     return rand();
 }
 
+//函数测试
 int mainfun1(void) {
     int myarray[10];
     /* getNextRandomValue 不能加括号，否则无法编译，因为加上括号之后相当于传入此参数时传入了 int , 而不是函数指针*/
--- bitmap-compress/src/main/cpp/native-lib.cpp
+++ bitmap-compress/src/main/cpp/native-lib.cpp
@@ -18,6 +18,8 @@ struct my_error_mgr {
     struct jpeg_error_mgr pub;
     jmp_buf setjmp_buffer; /* for return to caller */
 };
+
+
 typedef struct my_error_mgr *my_error_ptr;
 
 METHODDEF(void) my_error_exit(j_common_ptr cinfo) {
